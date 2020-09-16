f1c31d3e022f9a9673f0cd396ee521c1554aff7d
rachsc <raquelsc94@gmail.com>
2020-09-16 11:41:24 +0200

    component for farce recognition

---
+++ SmartFaceRecognition/smartsoft/build/CMakeFiles/SmartFaceRecognition_distclean.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SmartFaceRecognition_distclean"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/SmartFaceRecognition_distclean.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
